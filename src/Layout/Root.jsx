82b38f09d1a9bceff6eea2e4f595eaacbb132aa6
unknown <mehedytouhidurzaman@gmail.com>
2024-11-29 23:40:22 +0600

    banner section is completed

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import NavBar from '../Pages/Shared/NavBar';
+import Footer from '../Pages/Shared/Footer';
 
 const Root = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Shared/NavBar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const NavBar = () => {
+    const navLinks = (
+      <>
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/contactUs"}>Contact Us</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/ourMenu"}>Our Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/dashboard"}>Dashboard</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/ourShop"}>Our Shop</NavLink>
+        </li>
+      </>
+    );
+    return (
+      <div className="navbar fixed z-30  bg-black opacity-65 text-white font-bold">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl uppercase">Bistro Boss</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+    );
+};
+
+export default NavBar;
